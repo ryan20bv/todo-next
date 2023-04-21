77158163f06f428a88be57dfd29f538094317ed0
unknown <ryan20bv@gmail.com>
2023-04-22 07:17:28 +0800

    add li

--- src/components/task/TaskDetail.tsx
+++ src/components/task/TaskDetail.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { ITask } from "@/DUMMY_DATA/MODEL";
+import { TrashIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 
 interface propsType {
 	selectedTodo: {
@@ -13,9 +14,51 @@ const TaskDetail: React.FC<propsType> = ({ selectedTodo }) => {
 	const { todoDetail, index } = selectedTodo;
 
 	return (
-		<div>
-			<span>{index + 1 + ". "}</span> {todoDetail.name}
-		</div>
+		<main className=' w-full  p-4'>
+			<header>
+				<span>{index + 1 + ". "}</span> {todoDetail.name}
+			</header>
+			<section className=' bg-white w-full h-96 border border-black p-4 overflow-y-scroll'>
+				<h3>Details</h3>
+				<ul className='py-2 px-4 list-disc'>
+					<div>
+						<li className='pb-2'>
+							<p>
+								science books science books science books science books science books
+								science books science books science books
+							</p>
+						</li>
+						<button>
+							<PencilSquareIcon className='text-blue-600 h-6' />
+						</button>
+					</div>
+
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+					<li>science books</li>
+				</ul>
+			</section>
+			<form action=''>
+				<input
+					type='text'
+					placeholder='add new detail'
+				/>
+				<button>Add</button>
+			</form>
+		</main>
 	);
 };
 
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -6,7 +6,7 @@ interface propsTypes {
 
 const Card: React.FC<propsTypes> = (props) => {
 	return (
-		<main className=' pt-2 pb-6 flex justify-center '>
+		<main className=' pt-2 pb-6 flex justify-center border border-red-400'>
 			<div className='flex flex-col items-center w-[90%] sm:w-96 border border-black'>
 				<section className='bg-[#AF7EEB] w-full py-2 px-3 text-white text-center'>
 					<h1>TODO nextJS</h1>
