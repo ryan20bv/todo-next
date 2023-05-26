5fd5f9972ffe2c6a6c0924bc7a2545187321e4cc
unknown <ryan20bv@gmail.com>
2023-05-26 10:58:15 +0800

    create new route subtask

---
+++ src/pages/t/[maintaskname]/[subtaskname].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubtaskPage = () => {
+	return <div>Subtask Page</div>;
+};
+
+export default SubtaskPage;
---
+++ src/pages/t/[maintaskname]/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Card from "@/components/ui/Card";
+import CardHeader from "@/components/ui/CardHeader";
+
+const MainTaskPage = () => {
+	const router = useRouter();
+	console.log(router);
+	return (
+		<Card>
+			<CardHeader
+				title='Loading...'
+				from='index'
+			/>
+			<p>Loading</p>
+		</Card>
+	);
+};
+
+export default MainTaskPage;
