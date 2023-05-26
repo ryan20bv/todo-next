ab4e9e7746059a2099c02c7109a228609dc9f4f9
unknown <ryan20bv@gmail.com>
2023-05-26 11:13:58 +0800

    use combine reducers in  index store

--- src/reduxToolkit/indexStore/indexStore.ts
+++ src/reduxToolkit/indexStore/indexStore.ts
@@ -1,17 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import { useDispatch, useSelector } from "react-redux";
 import todoSlice from "../todo/todo-slice/todoSlice";
 import detailTodoSlice from "../todo/todo-slice/detailSlice";
 import authSlice from "../auth/auth-slice/authSlice";
 import personalTodoSlice from "../personal/personal-slice/personalTodoSlice";
 
+const reducers = combineReducers({
+	todoReducer: todoSlice.reducer,
+	detailReducer: detailTodoSlice.reducer,
+	authReducer: authSlice.reducer,
+	personalTodoReducer: personalTodoSlice.reducer,
+});
+
 const indexStore = configureStore({
-	reducer: {
-		todoReducer: todoSlice.reducer,
-		detailReducer: detailTodoSlice.reducer,
-		authReducer: authSlice.reducer,
-		personalTodoReducer: personalTodoSlice.reducer,
-	},
+	reducer: reducers,
 });
 
 export type RootState = ReturnType<typeof indexStore.getState>;
