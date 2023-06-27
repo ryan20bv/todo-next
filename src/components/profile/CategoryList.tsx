91ac9557cbb06c22a055aa3569dc7c3a88486af7
unknown <ryan20bv@gmail.com>
2023-06-27 20:04:48 +0800

    update category list

--- src/components/profile/CategoryList.tsx
+++ src/components/profile/CategoryList.tsx
@@ -9,6 +9,7 @@ import AddForm from "../ui/AddForm";
 import SendingData from "../ui/SendingData";
 import CategoryItem from "./CategoryItem";
 import ConfirmationModal from "../ui/ConfirmationModal";
+import EditForm from "../ui/EditForm";
 
 // import category custom hooks
 import useCategoryHook from "@/customHooks/use-categoryHook";
@@ -38,6 +39,11 @@ const CategoryList: React.FC<PropsType> = ({ onToggle }) => {
 		isUpdatingCategory,
 		categoryMessage,
 		currentCategory,
+		editCategoryHandler,
+		isEditingCategory,
+		categoryToEdit,
+		cancelEditCategoryHandler,
+		confirmEditCategoryHandler,
 	} = useCategoryHook();
 
 	useEffect(() => {
@@ -53,25 +59,38 @@ const CategoryList: React.FC<PropsType> = ({ onToggle }) => {
 
 	return (
 		<section className='w-[97%] text-center bg-white  border-b-2 border-black absolute  top-20 p-2 h-[60%]  '>
-			<div className='mb-4'>
-				{!isAddingCategory && (
-					<h1
-						className='text-white bg-green-400 w-3/5 m-auto border border-green-800 rounded-2xl'
-						onClick={toggleAddingCategoryHandler}
-					>
-						ADD New Category +
-					</h1>
-				)}
-				{isSendingData && <SendingData />}
+			{!isEditingCategory && (
+				<div className='mb-4'>
+					{!isAddingCategory && (
+						<h1
+							className='text-white bg-green-400 w-3/5 m-auto border border-green-800 rounded-2xl'
+							onClick={toggleAddingCategoryHandler}
+						>
+							ADD New Category +
+						</h1>
+					)}
+					{isSendingData && <SendingData />}
 
-				{isAddingCategory && !isSendingData && (
-					<AddForm
-						onAddHandler={addNewCategoryHandler}
-						placeHolder='Add new category'
-					/>
-				)}
-			</div>
-
+					{isAddingCategory && !isSendingData && (
+						<AddForm
+							onAddHandler={addNewCategoryHandler}
+							placeHolder='Add new category'
+						/>
+					)}
+				</div>
+			)}
+			{isEditingCategory && (
+				<div className='my-2'>
+					{isUpdatingCategory && <SendingData />}
+					{!isUpdatingCategory && (
+						<EditForm
+							itemToEdit={categoryToEdit.categoryName}
+							confirmEditing={confirmEditCategoryHandler}
+							onCancelEditing={cancelEditCategoryHandler}
+						/>
+					)}
+				</div>
+			)}
 			<ul className='border border-black overflow-y-scroll h-[85%]'>
 				{!categoryList ||
 					(categoryList.length === 0 && <div>Category List is Empty!</div>)}
@@ -87,6 +106,7 @@ const CategoryList: React.FC<PropsType> = ({ onToggle }) => {
 							idOfToggleToOpenMoreAction={idOfToggleToOpenMoreAction}
 							onSetToDelete={deleteCategoryHandler}
 							currentCategory={currentCategory}
+							onSetToEdit={editCategoryHandler}
 						/>
 					))}
 			</ul>
