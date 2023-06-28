cfa5488134ff47d7ca6fec310a8ba85d5a067519
unknown <ryan20bv@gmail.com>
2023-06-28 08:47:48 +0800

    remove special character and capitalize first letter

--- src/components/ui/AddForm.tsx
+++ src/components/ui/AddForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { PlusCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 
 interface propsTypes {
@@ -8,26 +8,55 @@ interface propsTypes {
 
 const AddForm: React.FC<propsTypes> = ({ onAddHandler, placeHolder }) => {
 	const inputRef = useRef<HTMLInputElement>(null);
+	const [inputValue, setInputValue] = useState<string>("");
+
+	const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+		console.log(e.currentTarget.value.trim());
+		const regex = /[^a-zA-Z0-9-_ ]/g;
+		const trimmedValue = e.currentTarget.value.trimStart().replace(regex, "");
+		const capitalizeFirstCharacter = (str: string): string => {
+			if (str.length === 0) {
+				return str; // Return empty string if input is empty
+			}
+			const firstChar = str.charAt(0).toUpperCase(); // Get the first character and capitalize it
+			const restOfString = str.slice(1); // Get the remaining characters of the string
+
+			return firstChar + restOfString; // Return the capitalized string
+		};
+
+		const capitalizedString = capitalizeFirstCharacter(trimmedValue);
+
+		setInputValue(capitalizedString);
+	};
 
 	const submitTodoHandler = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		const enteredInput = inputRef.current?.value;
-		if (!enteredInput || enteredInput?.trim() === "") {
+		// const enteredInput = inputRef.current?.value;
+		// if (!enteredInput || enteredInput?.trim() === "") {
+		// 	console.log("no value");
+		// 	return;
+		// }
+
+		if (!inputValue || inputValue?.trim().length === 0) {
 			console.log("no value");
 			return;
 		}
 
-		onAddHandler(enteredInput);
-		inputRef.current.value = "";
+		console.log(inputValue.trim());
+		console.log(inputValue.trim().length);
+
+		// onAddHandler(enteredInput);
+		// inputRef.current.value = "";
 	};
 
 	const cancelAddHandler = () => {
-		const enteredInput = inputRef.current?.value;
-		if (!enteredInput || enteredInput?.trim() === "") {
-			console.log("no value");
-			return;
-		}
-		inputRef.current.value = "";
+		// const enteredInput = inputRef.current?.value;
+		// if (!enteredInput || enteredInput?.trim() === "") {
+		// 	console.log("no value");
+		// 	return;
+		// }
+		// inputRef.current.value = "";
+		setInputValue("");
 	};
 
 	return (
@@ -41,9 +70,11 @@ const AddForm: React.FC<propsTypes> = ({ onAddHandler, placeHolder }) => {
 					type='text'
 					placeholder={placeHolder}
 					required
-					ref={inputRef}
+					// ref={inputRef}
 					className='py-1 px-2 focus:outline-none w-[85%] border border-black '
 					id={`add_input`}
+					onChange={changeHandler}
+					value={inputValue}
 					// data-testid={`filter_active`}
 				/>
 
